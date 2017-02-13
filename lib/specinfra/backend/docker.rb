1538e46535522438b523d72b1c2ec46483b87c8c
Colin Hebert <chebert@atlassian.com>
2017-02-14 09:47:38 +1100

    warn the cmd

--- lib/specinfra/backend/docker.rb
+++ lib/specinfra/backend/docker.rb
@@ -87,6 +87,7 @@ module Specinfra
 
       def docker_run!(cmd, opts={})
         opts.merge!(get_config(:docker_container_exec_options) || {})
+        warn cmd
         stdout, stderr, status = @container.exec([cmd], opts)
 
         CommandResult.new :stdout => stdout.join, :stderr => stderr.join,
